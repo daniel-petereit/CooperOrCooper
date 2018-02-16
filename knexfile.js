32f8203bc1c1a27f682800121592b4582780d846
Daniel Petereit <danielpetereit@Daniels-MacBook-Air-2.local>
2018-02-16 11:05:34 -0800

    add production

--- knexfile.js
+++ knexfile.js
@@ -8,5 +8,8 @@ module.exports = {
     client: 'pg',
     connection: `postgres://localhost:5432/Q2`
   },
-  production: {}
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
